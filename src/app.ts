de680bf0f43f793d06cf4940dccce6952b0862aa
SMNoushan <noushan.md@gmail.com>
2025-07-31 23:04:20 +0600

    updated: app with redisStore for session management + updated: server with better error handling

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import cors from "cors";
 import passport from "passport";
 import router from "./app/routes";
@@ -5,30 +6,39 @@ import config from "./app/config";
 import "./app/middlewares/passport";
 import cookieParser from "cookie-parser";
 import expressSession from "express-session";
+import { redisStore } from "./app/config/redis";
 import notFound from "./app/middlewares/notFound";
 import express, { Application, Request, Response } from "express";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 
+app.set("trust proxy", 1); // Trust first proxy for secure cookies if behind a reverse proxy
+app.use(cookieParser());
+app.use(cors({ origin: config.CORS_ORIGIN, credentials: true }));
+app.use(express.json());
 app.use(
   expressSession({
+    store: redisStore,
     secret: config.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      httpOnly: true,
+      secure: config.NODE_ENV === "production",
+      sameSite: config.NODE_ENV === "production" ? "none" : "lax",
+    },
   })
 );
-// app.set("trust proxy", 1); // Trust first proxy for secure cookies if behind a reverse proxy
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.json());
-app.use(cookieParser());
-app.use(cors({ origin: config.CORS_ORIGIN, credentials: true }));
 
 app.use("/api", router);
 
 app.get("/", async (req: Request, res: Response) => {
-  res.send("Server is running successfully");
+  // res.send("Server is running successfully");
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
 app.use(globalErrorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,48 @@
+/* eslint-disable no-console */
 import app from "./app";
 import { Server } from "http";
-import mongoose from "mongoose";
 import config from "./app/config";
+import connectMongoDB from "./app/config/db";
+import { connectRedis } from "./app/config/redis";
 
 let server: Server;
 
-async function main() {
+async function startServer() {
   try {
-    await mongoose.connect(config.DB_URL);
-
     server = app.listen(config.PORT, () => {
-      console.log("Server is running on port:", config.PORT);
+      console.log("🔥 Server is running on port:", config.PORT);
     });
   } catch (error) {
-    console.log(error);
+    console.error("❌ Failed to start server...", error);
   }
 }
 
-main();
+(async () => {
+  await connectRedis();
+  await connectMongoDB();
+  await startServer();
+})();
 
-process.on("unhandledRejection", () => {
+process.on("SIGTERM", () => {
+  console.log("❌ SIGTERM Signal Received! Shutting down...");
   if (server) server.close(() => process.exit(1));
   process.exit(1);
 });
 
-process.on("uncaughtException", () => {
-  console.log("Uncaught Exception! Shutting down...");
+process.on("SIGINT", () => {
+  console.log("❌ SIGINT Signal Received! Shutting down...");
+  if (server) server.close(() => process.exit(1));
+  process.exit(1);
+});
+
+process.on("unhandledRejection", err => {
+  console.error("❌ Unhandled Rejection! Shutting down...", err);
+  if (server) server.close(() => process.exit(1));
+  process.exit(1);
+});
+
+process.on("uncaughtException", err => {
+  console.error("❌ Uncaught Exception! Shutting down...", err);
+  if (server) server.close(() => process.exit(1));
   process.exit(1);
 });
